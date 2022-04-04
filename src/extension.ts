9d94d48a2099788f732f510a4f9f43e3ac69b777
Yasufumi Ogawa <yasufum.o@gmail.com>
2022-04-05 02:32:05 +0900

    Done to get test path
    Signed-off-by: Yasufumi Ogawa <yasufum.o@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,98 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import internal = require('stream');
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "openstack-tox" is now active!');
+	// TODO(yasufum): Replace with meaningful msg.
+	console.log('"openstack-tox" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+	const NEWLINE_REGEX = /\r\n|\r|\n/;
+
+	// Get all functions and classes in the file.
+	let _getAllFunctions = (): Array<Map<string, any>> => {
+		const activeEditor = vscode.window.activeTextEditor;
+		var fileContents = activeEditor?.document.getText();
+		var contentsLines = fileContents?.split(NEWLINE_REGEX);
+
+		var lines = new Array();
+
+		if (contentsLines) {
+			var cnt = 1;  // current line in for loop
+
+			// Find test functions
+			for (var line of contentsLines) {
+				if (line.match(/^\s*?(def|class) /)) {
+					const actualDef = line.replace(/^\s+/i, "");
+					const fname = actualDef
+						.replace(/^def /i, "")
+						.replace(/^class /i, "")
+						.split("(")[0];
+
+					// TODO(yasufum): Revise with smarter way of
+					// initialization.
+					var tmp: Map<string, any> = new Map();
+					tmp.set("num", cnt);
+					tmp.set("fname", fname);
+					tmp.set("depth", line.length - actualDef.length);
+					if (actualDef.startsWith("class")) {
+						tmp.set("isClass", true);
+					} else {
+						tmp.set("isClass", false);
+					}
+					lines.push(tmp);
+				}
+				cnt++;
+			}
+		}
+		return lines;
+	};
+
+	// Get the name of function on which cursor is.
+	let _getCurrentFunction = (): string => {
+		const activeEditor = vscode.window.activeTextEditor;
+
+		const lineAt = activeEditor?.selection.active.line! + 1;
+		if (lineAt) {
+			const allFuncs = _getAllFunctions().reverse();
+
+			for (var func of allFuncs) {
+				if (func.get("num") <= lineAt) {
+					if (func.get("fname").startsWith("test_") ||
+						func.get("isClass") === true)
+					{
+						return func.get("fname");
+					}
+				}
+			}
+		}
+		return "";
+	};
+
+	let _getTestFunction = (): string => {
+		return  _getCurrentFunction();
+	};
+
+	let getTestPath = (): string | undefined => {
+		const filePath = vscode.window.activeTextEditor?.document
+			.uri.path.split(".")[0];
+		const wsPath = vscode.workspace.workspaceFolders![0].uri.path;
+
+		const filePathAry = filePath!.split("/");
+		const wsPathAry = wsPath!.split("/");
+		let a = new Array();
+		for (var i = wsPathAry.length; i < filePathAry.length; i++) {
+			a.push(filePathAry[i]);
+		}
+		let testPath = a.join(".");
+
+		let curFun= _getCurrentFunction();
+		if (curFun!== "") {
+			testPath = testPath + "." + curFun;
+		}
+		return testPath;
+	};
 
 	let myfunc = (err: string, stdout: string, stderr: string) => {
 			console.log('stdout: ' + stdout);
@@ -28,6 +108,7 @@ export function activate(context: vscode.ExtensionContext) {
 			cp.exec('tox -h', myfunc);
 	};
 
+	// Setup all command names and its definitions here.
 	let commands = [
 		{
 			"name": "openstack-tox.run-tox",
@@ -39,20 +120,32 @@ export function activate(context: vscode.ExtensionContext) {
 		{ 
 			"name": "openstack-tox.run-debug",
 			"func": () => {
-				vscode.window.showInformationMessage('Run tox debug command');
+				var testPath = getTestPath();
+				if (testPath) {
+					vscode.window.showInformationMessage(testPath);
+				}
 			}
 		},
 		{
-			 "name": "openstack-tox.run-debug-atline",
+			 "name": "openstack-tox.run-test",
 			 "func": () => {
-				vscode.window.showInformationMessage('Run tox debug command atline');
+				vscode.window.showInformationMessage(
+					'Run tox test command atline');
+			}
+		},
+		{
+			 "name": "onDebug",
+			 "func": () => {
+				vscode.window.showInformationMessage(
+					'Run tox test command atline');
 			}
 		},
 	];
 
 	for (let i = 0; i < commands.length; i++) {
 		let cmd = commands[i];
-		let disposable = vscode.commands.registerCommand(cmd["name"], cmd["func"]!);
+		let disposable = vscode.commands.registerCommand(
+			cmd["name"], cmd["func"]!);
 		context.subscriptions.push(disposable);
 	}
 }
